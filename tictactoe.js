e901483e0ca85712e83bbec6911f341f65bec7f4
Demetrio <dmitr77@gmail.com>
2017-09-18 17:34:27 -0500

    Checking for winner complete

--- tictactoe.js
+++ tictactoe.js
@@ -1,40 +1,82 @@
 var tictactoe = function() {
 
-    let stdin = process.openStdin();
-    let firstPlayer = true
+  let stdin = process.openStdin();
+  let firstPlayer = true
+  let player = firstPlayer? 'One' : 'Two'
+  let bDisplay
 
-    let board = {
-      'a': {1: null, 2: null, 3:null},
-      'b': {1: null, 2: null, 3:null},
-      'c': {1: null, 2: null, 3:null}
-    }
+  // let b = {
+  //   'a': {'1': '  ', '2': '  ', '3': '  '},
+  //   'b': {'1': '  ', '2': '  ', '3': '  '},
+  //   'c': {'1': '  ', '2': '  ', '3': '  '}
+  // }
 
-    let place = function(x, y) {
+  let b = [['  ', '  ', '  '], ['  ', '  ', '  '], ['  ', '  ', '  ']]
+
+  let updateDisplay = function() {
+    bDisplay = `${b['0']['0']}|${b['0']['1']}|${b['0']['2']}\n---------\n${b['1']['0']}|${b['1']['1']}|${b['1']['2']}\n---------\n${b['2']['0']}|${b['2']['1']}|${b['2']['2']}`
+  }
+
+  let place = function(x, y, cb) {
+    if (b[x][y] === '  ') {
       let XorO = firstPlayer? 'X' : 'O'
-      board[x][y] = XorO
+      b[x][y] = XorO
+      cb()
+    } else {
+      console.log('square taken try again')
     }
+  }
 
-//     let text = prompt('hey')
-//     console.log('u said', test)
-// console.log(board)
+  let isWinner = function() {
+    for (var i = 0; i < 3; i++) {
+
+        if(b[i][0].toString() == 'X' && b[i][1].toString() == 'X' && b[i][2].toString() == 'X') {
+            console.log('Player One Wins')
+        }
+        if(b[i][0].toString() == 'O' && b[i][1].toString() == 'O' && b[i][2].toString() == 'O') {
+            console.log('Player Two Wins')
+        }
+        if(b[0][i].toString() == 'X' && b[1][i].toString() == 'X' && b[2][i].toString() == 'X') {
+            console.log('Player One Wins')
+        }
+        if(b[0][i].toString() == 'O' && b[1][i].toString() == 'O' && b[2][i].toString() == 'O') {
+            console.log('Player Two Wins')
+        }
+      }
+      if(b[0][0].toString() == 'X' && b[1][1].toString() == 'X' && b[2][2].toString() == 'X') {
+          console.log('Player One Wins')
+      }
+      if(b[0][0].toString() == 'O' && b[1][1].toString() == 'O' && b[2][2].toString() == 'O') {
+          console.log('Player Two Wins')
+      }
+      if(b[0][2].toString() == 'X' && b[1][1].toString() == 'X' && b[2][0].toString() == 'X') {
+          console.log('Player One Wins')
+      }
+      if(b[0][2].toString() == 'O' && b[1][1].toString() == 'O' && b[2][0].toString() == 'O') {
+          console.log('Player Two Wins')
+      }
+  }
 
 
   // data listneer
-  console.log(board)
+  updateDisplay()
+  console.log(bDisplay)
+  console.log(`Player\'s ${player} turn`)
+
   stdin.addListener("data", function(d) {
+    let coords = d.toString().trim().split('')
+    console.log(coords)
 
-    place('a',1)
+    place(coords[0], coords[1], ()=>{
+      updateDisplay()
+      isWinner()
+      console.log(bDisplay)
+      firstPlayer? firstPlayer = false : firstPlayer = true
+      player = firstPlayer? 'One' : 'Two'
+      console.log(`Player\'s ${player} turn`)
+    })
 
-    console.log(board)
-    // note:  d is an object, and when converted to a string it will
-    // end with a linefeed.  so we (rather crudely) account for that
-    // with toString() and then trim()
-    console.log("you entered: [" + d.toString().trim() + "]");
-    firstPlayer? firstPlayer = false : firstPlayer = true 
-
-    });
-
-
+  });
 }
 
 tictactoe()
